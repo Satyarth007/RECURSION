3b47876c293568d501e57a5bbb82fba42b2981e2
SATYARTH TIWARI <satyarthtiwari123@gmail.com>
2022-06-16 22:40:51 +0530

    Update linearSearch.java

--- linearSearch.java
+++ linearSearch.java
@@ -2,15 +2,15 @@
 public class linearSearch {
 	public static boolean search(int[] arr,int n,int key) {
 		if(n==-1) {
-			return false;
+			return false; // because u reached the front end and didn't find the element
 		}
-		return ((arr[n]==key) || search(arr,n-1,key));
+		return ((arr[n]==key) || search(arr,n-1,key)); // return true if anywhere it finds the elements
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// RECURSIVE ALGORITHM FOR LINEAR SEARCH
 		int arr[]= {8,6,4,99,1,41,22,35,7};
-		int key=100;
+		int key=41; // try key=100 -> u will get false
 		System.out.println(search(arr,arr.length-1 ,key));
 
 	}
